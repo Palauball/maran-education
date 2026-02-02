0636374123f975f14fdb10194ef6104e97b2fb46
JeremyZXi <zjeremy0710@gmail.com>
2026-02-02 13:40:05 +0800

    new offer

--- src/components/SuccessCases.tsx
+++ src/components/SuccessCases.tsx
@@ -41,7 +41,16 @@ const SuccessCases = () => {
       logo: 'https://ascintranet.osu.edu/themes/custom/asc_bootstrap_bux/images/osu-logos/navbar/osu-navbar.svg',
       quote: '非常优秀的机构老师们, 尤其是文书指导老师! 太专业了!',
       achievement: ''
-    }
+    },
+    {
+      name: 'S同学',
+      university: 'University of Virginia',
+      major: 'Philosophy, Politic, and Law',
+      year: '2030',
+      logo: 'https://www.freelogovectors.net/wp-content/uploads/2022/04/wesleyan_cardinals_logo_freelogovectors.net_.png',
+      quote: '感谢马然留学团队的专业指导，从文书撰写到面试准备，每一步都给予了我最大的帮助。',
+      achievement: ''
+    },
   ];
 
   const universityNames = Array.from(new Set(cases.map((caseItem) => caseItem.university)));
